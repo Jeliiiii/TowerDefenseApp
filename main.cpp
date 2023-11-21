d878e9d36e468b696e90860ea10889d66e211184
Lorenzo <116020466+lorenzo-molina@users.noreply.github.com>
2023-11-21 09:56:58 +0100

    zdazda

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
+    RenderWindow window(VideoMode(WIN_WIDTH, WIN_HEIGHT), "Casse-briques");
+
+    window.setVerticalSyncEnabled(true);
+
+    while (window.isOpen())
+    {
+        //EVENT
+        while (window.pollEvent(event))
+        {
+
+        }
+
+        //UPDATE
+
+        //DRAW
+        window.clear(Color::Black);
+
+        // C'est ici qu'on dessine les éléments du jeu   
+
+        window.display();
+
+        //End of loop
+    }
     return 0;
 }
--- main.h
+++ main.h
@@ -1,1 +1,9 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+constexpr auto WIN_WIDTH = 1280;
+constexpr auto WIN_HEIGHT = 720;
