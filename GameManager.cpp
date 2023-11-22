330cb750237faa5ba34c5864756b3422033dbd22
Pepitovador <92396135+Rhivan@users.noreply.github.com>
2023-11-22 18:20:01 +0100

    hhh

--- GameManager.cpp
+++ GameManager.cpp
@@ -3,7 +3,7 @@
 
 GameManager::GameManager(int w, int h)
 {
-    window.create(sf::VideoMode(w, h), "how are your balls");
+    window.create(sf::VideoMode(w, h), "Tower Defender");
     window.setFramerateLimit(60);
     window.setVerticalSyncEnabled(true);
 }
@@ -15,6 +15,8 @@ GameManager::~GameManager()
 
 void GameManager::loop()
 {
+    int cellSize = 80;
+    Map gameMap;
     while (run && window.isOpen())
     {
         sf::Event event;
@@ -23,6 +25,21 @@ void GameManager::loop()
             if (event.type == sf::Event::Closed)
             {
                 window.close();
+            }
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                {
+					window.close();
+				}
+			}
+
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                int cellX = mousePosition.x / cellSize;
+                int cellY = mousePosition.y / cellSize;
+                gameMap.placeTower(cellX, cellY);
             }
         }
 
